ba668ac249c81c722be319e93b9548d4c1a00fe4
wilson <yxw_android@163.com>
2019-12-17 10:59:07 +0800

    实现mvvm

---
+++ mvvm/src/main/java/com/wilson/mvvm/MainActivity.java
@@ -0,0 +1,36 @@
+package com.wilson.mvvm;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.wilson.mvvm.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        LoginViewModel loginViewModel = ViewModelProviders.of(this).get(LoginViewModel.class);
+        binding.setLoginViewModel(loginViewModel);
+        binding.setLifecycleOwner(this);
+
+
+        loginViewModel.getUser().observe(this, new Observer<User>() {
+            @Override
+            public void onChanged(@Nullable User user) {
+                if (user.getEmail().length() > 0 || user.getPassword().length() > 0)
+                    Toast.makeText(getApplicationContext(), "email : " + user.getEmail() + " password " + user.getPassword(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
